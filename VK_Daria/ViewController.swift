80075f8ddc550c3ef325d2b19cac9f42f070da40
Dasha <mar4enkodv@gmail.com>
2020-12-24 14:02:28 +0300

    Initial Commit

---
+++ VK_Daria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_Daria
+//
+//  Created by Dasha Marchenko on 24.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
